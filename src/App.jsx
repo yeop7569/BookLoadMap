30532c653e15ba70afb1dc57127f0573b3a40d97
Yeop <yeop8522@gmail.com>
2025-06-16 22:55:56 +0900

    Feat: Footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
 import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
     <Header />
     <MainPage />
+    <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-base-200 text-base-content py-6 px-8 fixed bottom-0 left-0">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <aside className="flex flex-col gap-2">
+          <p className="text-lg font-bold">BookLoadMap</p>
+          <p>당신의 책 추천 로드맵<br />© 2025 BookLoadMap. All rights reserved.</p>
+        </aside>
+        <nav className="flex flex-col gap-2">
+          <h6 className="footer-title">Links</h6>
+          <a className="link link-hover">홈</a>
+          <a className="link link-hover">로드맵</a>
+          <a className="link link-hover">로그인</a>
+          <a className="link link-hover">문의</a>
+        </nav>
+      </div>
+    </footer>
+  );
+}
