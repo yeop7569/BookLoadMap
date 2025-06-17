20588831f09990fd464ea48f889a6a31ab9005b3
Yeop <yeop8522@gmail.com>
2025-06-17 23:49:48 +0900

    Fix: 라우터 수정2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 // src/components/Header.jsx
+import { Link } from 'react-router-dom';
 export default function Header() {
     return (
       <header className="bg-white shadow-md p-4 flex justify-between items-center">
-        <h1 className="text-xl font-bold text-blue-600">BookMap</h1>
+           <Link to="/">
+        <button className="text-xl font-bold text-blue-600">BookMap</button>
+        </Link> 
         <nav className="space-x-4">
           <button className="text-gray-700 hover:text-blue-500">로그인</button>
           <button className="text-gray-700 hover:text-blue-500">회원가입</button>
